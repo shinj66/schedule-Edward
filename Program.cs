5b4b8805f8279b421e94a2723db51b0898a07efa
princessbantilan35-max <shanleylim13@gmail.com>
2026-02-28 11:12:20 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Formats.Asn1;
 using System.Security.Cryptography.X509Certificates;
 
 namespace schedule
@@ -8,7 +9,7 @@ namespace schedule
     {
         static void Main(string[] args)
         {
-            
+
             int num;
             while (true)
             {
@@ -19,28 +20,41 @@ namespace schedule
                 Console.WriteLine("3. Filipino. ");
                 Console.WriteLine("4. Integrative Programming. ");
                 Console.WriteLine("5. Computer Programming. ");
-                Console.WriteLine("6. Exit ");
-                num = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("6. Exit\n");
+                Console.Write("Enter a number from 1-6: ");
+                
 
-                if (num == 1)
+                if (!int.TryParse(Console.ReadLine(), out num))
+
                 {
-                    Console.WriteLine("OOP \nMonday: 8:30 AM - 12:30 PM \nSaturday: 2:30 PM - 5:30 PM");
+                    Console.WriteLine("Invalid input, Please choose a number in the schedule: ");
+                    continue;
+                }
+
+                else if (num == 1)
+                {
+                    Console.WriteLine("\nOOP \nMonday: 8:30 AM - 12:30 PM \nSaturday: 2:30 PM - 5:30 PM");
+                    Console.WriteLine("Professor: Mr. Ed \nRoom 204\n");
                 }
                 else if (num == 2)
                 {
-                    Console.WriteLine("PATHFIT \nMonday: 2:00 PM - 5:00 PM \nWednesday: 8:00 AM - 12:00 PM");
+                    Console.WriteLine("\nPATHFIT \nMonday: 2:00 PM - 5:00 PM \nWednesday: 8:00 AM - 12:00 PM");
+                    Console.WriteLine("Professor: Mrs. Apostol \nRoom 205\n");
                 }
                 else if (num == 3)
                 {
-                    Console.WriteLine("Filipino \nWednesday: 2:00 PM - 5:00 PM \nFriday: 2:00 PM - 5:00 PM");
+                    Console.WriteLine("\nFilipino \nWednesday: 2:00 PM - 5:00 PM \nFriday: 2:00 PM - 5:00 PM");
+                    Console.WriteLine("Professor: Mr. Mislan \nRoom 308\n");
                 }
                 else if (num == 4)
                 {
-                    Console.WriteLine("Integrative Programming \nSunday: 8:30 AM - 12:30 PM \nSunday: 12:30 PM - 3:30 PM");
+                    Console.WriteLine("\nIntegrative Programming \nSunday: 8:30 AM - 12:30 PM \nSunday: 12:30 PM - 3:30 PM");
+                    Console.WriteLine("Professor: Ms. Indaleen\nRoom 207\n");
                 }
                 else if (num == 5)
                 {
-                    Console.WriteLine("Computer Programming \nFriday: 8:30 AM - 12:30 PM \nSaturday: 8:30 AM - 1:30 PM");
+                    Console.WriteLine("\nComputer Programming \nFriday: 8:30 AM - 12:30 PM \nSaturday: 8:30 AM - 1:30 PM");
+                    Console.WriteLine("Professor: Mr. Rowell \nRoom 210\n");
                 }
                 else if (num == 6)
                 {
