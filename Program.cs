d50ba36e167526fad20c80e989aa6c3060f7382b
shinj66 <qqweredward@gmail.com>
2026-03-27 15:15:27 +0800

    .

--- Program.cs
+++ Program.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Formats.Asn1;
-using System.Security.Cryptography.X509Certificates;
 using ClassLibrary1;
 
 namespace schedule
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
-            Class1 schedule = new Class1();
-            schedule.sched();
+            ScheduleSystem Schedule = new ScheduleSystem();
+            Schedule.Sched();
+
         }
     }
 }
+
